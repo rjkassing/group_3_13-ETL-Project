877ff22eb2a2f953f31391b56b8b41a8c7adb88a
Rick Kassing <rkassing@gmail.com>
2020-12-03 21:44:58 -0500

    added finished files

---
+++ happy_region.sql
@@ -0,0 +1,5 @@
+CREATE TABLE happy_region (
+country VARCHAR, 
+	--FOREIGN KEY (country) REFERENCES happy_water(country),
+"Region" VARCHAR
+);
---
+++ happy_water.sql
@@ -0,0 +1,17 @@
+CREATE TABLE happy_water (
+country VARCHAR PRIMARY KEY,
+"Happiness_Rank" INT,
+"Happiness_Score" INT,
+"Whisker_high" INT,
+"Whisker_low" INT,
+"Economy_GDP_per_Capita" INT,
+"Family" INT,
+"Health_Life_Expectancy" INT,
+"Freedom" INT,
+"Generosity" INT,
+"Trust_Government_Corruption" INT,
+"Dystopia_Residual" INT,
+"Water_Index_2015" INT,
+"Water_Index_2016" INT,
+"Water_Index_2017" INT	
+);
